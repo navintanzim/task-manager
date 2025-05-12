b79e36986cd26e144993aa8da6fc213d44c66b0d
JIRA Developer <tanzim@ba-systems.com>
2025-05-13 02:24:45 +0600

    dashboard tables

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/tasks', [TaskController::class, 'store']);
     Route::get('/tasks', [TaskController::class, 'index']);
 
-    // return $request->user();
+    Route::get('/user', fn(Request $request) => $request->user());
 });
 
 Route::get('/test', function (Request $request) {
